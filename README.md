3366fc1318f56bff4fa766236baab8e17e0ae22a
Frode <Frode@Bagend.com>
2019-06-05 18:52:15 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## solid
+
+1) Single responsibility principle. 
+A class should only have a single responsibility.
+A class should only have one reason to change.
+
+
---
+++ src/main/java/com/example/demo/EmployeeV1.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+/**
+ * This class violates SRP.
+ * It has more than one responsibility
+ * It has more than one reason to change
+ */
+public class EmployeeV1 {
+
+    public void calcPay() {
+
+    }
+
+    public void reportHours() {
+
+    }
+
+    public void writeEmployee() {
+
+    }
+}
